64b8975a1360c556e38b29c479b47fa4f5e475aa
Vishal <Vishal>
2022-11-20 23:34:15 +0530

    This is my second commit

---
+++ REVISE.cpp
@@ -0,0 +1,145 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cout<<"Enter your age please"<<endl;
+//     cin>>age;
+//     if(age<18){
+//         cout<<"Your age is "<<age<<" and you can't drive"<<endl;
+//     }
+//     else{
+//         cout<<"Your age is "<<age<<" and you can drive a car"<<endl;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cout<<"Enter your age"<<endl;
+//     cin>>age;
+//     switch(age){
+//         case 17:
+//         cout<<"Your age is "<<age<<" so you can't drive"<<endl;
+//         break;
+//         case 20:
+//         cout<<"your age is "<<age<<" so you r valid to join"<<endl;
+// //         break;
+// //         case 30:
+// //         cout<<"Your age is "<<age<<" and you are experienced"<<endl;
+// //         break;
+// //         default:
+// //         cout<<"You entered invalid choice"<<endl;
+// //     }
+// //     return 0;
+// // 
+// // #include<iostream>
+// // using namespace std;
+// // int main(){
+// //     int number;
+// //     cout<<"Enter your number please vishal singh"<<endl;
+// //     cin>>number;
+// //     for(int i=0;i<=number;i++){
+// //         cout<<"The number you have entered is before that number is "<<endl;
+// //     }
+// //     if(number<=23){
+// //         cout<<"That number is best for the performing in the order of the notation but the things"<<cout<<endl;
+// //     }
+// //     return 0;
+    
+// // }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int value;
+//     cout<<"Enter the value of the number"<<endl;
+//     cin>>value;
+//     cout<<"The value you have written in the is"<<value<<endl;
+//         cout<<"The multiplicatoin table is "<<endl;
+//     for(int i=1;i<=10;i++){
+    
+//         cout<<value*i=
+//     }
+//     return 0;
+// }
+// #incldue<isotream>
+// using namespace std;
+// int main(){
+//     int number;
+//     cout<<"Enter the number"<<endl;
+
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int number,a=0,b=1,c;
+//     cout<<"Enter the vlaue of the number"<<endl;
+//     cin>>number;
+//     for(int i=1;i<=number;i++){
+//         cout<<a<<endl;
+//         c=a+b;
+//         a=b;
+//         b=c;
+//         c=a;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int fact,factorial=1;
+//     cout<<"Enter the value of the factorial number"<<endl;
+//     cin>>fact;
+//     for(int i=1;i<=fact;i++){
+//         factorial=factorial*i;
+//     }
+//     cout<<"The factorial of the given number is "<<factorial<<endl;
+//     return 0;
+// }
+//class and object using
+// #include<iostream>
+// using namespace std;
+// class base{
+//     int number;
+//     public:
+//     void factorial(){
+//         int fact=1;
+//         cout<<"Enter the value of the number"<<endl;
+//         cin>>number;
+//         for(int i=1;i<=number;i++){
+//             fact=fact*i;
+//         }
+//         cout<<"The factorial of the given number is "<<fact;
+//     }
+// }vishal;
+// int main(){
+//     vishal.factorial();
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Enter the value of the number please vishal singh"<<endl;
+//     cin>>number;
+//     cout<<
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int number;
+//     cout<<"Enter the value of the number please vishal sing"<<endl;
+//     cin>>number;
+//     cout<<"The value of the number you have written is "<<number<<" so thanks for response"<<endl;
+//     return 0;
+// }
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    cout<<"Enter your name please"<<endl;
+    cin>>name;
+    cout<<"your name is: "<<name<<endl;
+    return 0;
+}
